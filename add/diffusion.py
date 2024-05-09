935d43d7b337e796dea4404a865d3c9d4e4a7d2d
Lev Novitskiy <57654885+leffff@users.noreply.github.com>
2024-05-09 23:48:36 +0300

    Update diffusion.py

--- add/diffusion.py
+++ add/diffusion.py
@@ -47,11 +47,11 @@ def one_step_backward_diffusion_process(x, t, y, model, noise_scheduler, device:
     bs = x.shape[0]
     noise_scheduler.set_timesteps(num_inference_steps=num_timesteps)
     
-    model_input = noise_scheduler.scale_model_input(x, t)
+    model_input = noise_scheduler.scale_model_input(x, torch.tensor([t]))
 
     t_batch = torch.full(
         size=(bs,), 
-        fill_value=t.item(), 
+        fill_value=t, 
         dtype=torch.long
     ).to(device)
 
@@ -59,6 +59,6 @@ def one_step_backward_diffusion_process(x, t, y, model, noise_scheduler, device:
         model_input, t_batch, y, return_dict=False
     )[0]
 
-    x = noise_scheduler.step(noise_pred, t, x).pred_original_sample
+    x = noise_scheduler.step(noise_pred, torch.tensor([t]), x).pred_original_sample
 
     return x
